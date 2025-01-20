29cc4739269dffe9c0b41419f49b8bcf356cfed3
taemin6536 <xoals6536@naver.com>
2025-01-20 21:18:13 +0900

    feat: build jakarta 추가, product dto 분리, 책임과 의존을 지킴 service 진행 예정 추후 productCode 생성 로직 개발예정.

---
+++ src/main/java/com/v1/product/api/ProductController.java
@@ -0,0 +1,39 @@
+package com.v1.product.api;
+
+import com.v1.product.api.mapper.ProductCommandMapper;
+import com.v1.product.api.dto.request.CreateProductRequest;
+import com.v1.product.api.dto.response.ProductResponse;
+import com.v1.product.application.dto.command.CreateProductCommand;
+import com.v1.product.application.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+    private final ProductCommandMapper productCommandMapper;
+
+    @GetMapping(value = "/getAll")
+    public ResponseEntity<List<ProductResponse>> getProductList() {
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<Void> createProduct(
+            @RequestBody @Valid final CreateProductRequest createProductRequest
+    ) {
+        CreateProductCommand command = productCommandMapper.createProductRequestToCommand(createProductRequest);
+
+        productService.createProduct(command);
+
+        return ResponseEntity.created(URI.create("/product")).build();
+    }
+}
---
+++ src/main/java/com/v1/product/api/dto/response/ProductResponse.java
@@ -0,0 +1,13 @@
+package com.v1.product.api.dto.response;
+
+import com.v1.global.common.enums.Status;
+import com.v1.product.domain.model.ProductCode;
+
+public record ProductResponse(
+        Long id,
+        String name,
+        String description,
+        Status status,
+        ProductCode productCode
+) {
+}
